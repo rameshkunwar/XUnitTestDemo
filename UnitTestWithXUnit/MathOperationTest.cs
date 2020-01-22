415ab6dfd2208e032503faaca93657537aab15ba
Ramesh Kunwar <rameshkunwar@hotmail.com>
2020-01-22 21:37:40 +0100

    first commit

---
+++ UnitTestWithXUnit/MathOperationTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using XUnitTestDemo;
+
+namespace UnitTestWithXUnit
+{
+    public class MathOperationTest
+    {
+        [Fact]
+        public void Add_Two_Double_Number()
+        {
+            //Arrange
+            var num1 = 2.9;
+            var num2 = 4.4;
+            var expectedValue = 7.3;
+
+            //Act
+            var sum = MathOperation.Add(num1, num2);
+
+            //Assert
+            Assert.Equal(expectedValue, sum, 1);
+        }
+
+        [Fact]
+        public void Convert_String_To_Guid()
+        {
+            //arrange
+            var idAsString = "118a32f1-7737-45c7-8ef9-84e7808436f5";
+            var expectedValue = Guid.Parse(idAsString);
+
+            //Act
+            var guid = idAsString.ConvertStringToGuid();
+
+            //Assert
+            Assert.Equal(expectedValue, guid);
+        }
+    }
+}
---
+++ XUnitTestDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XUnitTestDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = MathOperation.Add(120.25, 254.25);
+            Console.WriteLine($"120.25 + 254.25 = {result}");
+
+            string id = Guid.NewGuid().ToString();
+            var guidId = id.ConvertStringToGuid();
+
+            Console.WriteLine(guidId);
+
+            Console.ReadLine();
+        }
+    }
+}
